5780e3b0bdbb96b811501b3d06261da710b0be14
huytv <huy8895@gmail.com>
2021-06-15 20:33:44 +0700

    Initial commit

---
+++ src/main/java/com/example/springbootangularfirebasepushnoti/SpringbootAngularFirebasePushnotiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootangularfirebasepushnoti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularFirebasePushnotiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAngularFirebasePushnotiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootangularfirebasepushnoti/SpringbootAngularFirebasePushnotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootangularfirebasepushnoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAngularFirebasePushnotiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
